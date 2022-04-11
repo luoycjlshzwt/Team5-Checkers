3131ef6839903a1727abce65a1f0f7dc0b9542e4
NA-0 <102358355+luoycjlshzwt@users.noreply.github.com>
2022-04-11 22:59:44 +0800

    Update widget.h

--- qtcode/widget.h
+++ qtcode/widget.h
@@ -19,19 +19,21 @@ class Widget : public QWidget
 
 public Q_SLOTS:
     void cbuttonpress();
+
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
     void mousePressEvent(QMouseEvent* ev);
-    bool islegal();
-    void int2pixel();   //将loc坐标转换为像素坐标
-    int pixel2int(QPointF& pixel);
+    int islegal();//判断移动是否合法
+
+    int pixel2int(QPointF pixel);
     void CheckerMove(CheckerButton*btn,QPointF p);
 
+    bool canJump(int x,int y);//排查是否可以进行下一次跳跃
+    void shouldSwitcht2f();
 protected:
     void paintEvent(QPaintEvent *);
-     void Mpe(QMouseEvent *event);
 
 private:
     Ui::Widget *ui;
@@ -39,19 +41,34 @@ private:
     void InitCheckerboard();
     QPointF loc[17][17];//棋盘每个位置的坐标
     bool isfill[17][17];//棋盘每个位置是否有棋子
-//    CheckerButton *rbtn[10],*bbtn[10];//红蓝双方棋子
     CheckerButton* btn[6][10]; //六方棋子
     int playernum; //棋手个数
     int flag;  //判断下棋方
-    QPointF chosen;//选中棋子位置
+
+
+    QPointF chosen;//选中的棋子
+    CheckerButton* checked;//被选中的棋子
+    bool haveJumped;
+    CheckerButton* jumped;
+
+
     int chosenloc[2];//选中棋子所在位置
-    CheckerButton* chosenbtn;//选中棋子
     bool ischosen=false;//是否有棋子被选中
-    QPointF obj;//目标位置？
-    int objloc[2];//目标位置坐标？
-    bool jumpmove;
+    bool isobjset=false;
+    QPointF obj;//目标位置
+    int objloc[2];//目标位置坐标
+    bool ischange=false;
+    int btnx,btny;//记录选中棋子初始位置
     QLabel* test;
+    QLabel* nowplayer;
     QPushButton* end;
+    bool shouldSwitch;
     mydialog1 *z;
+signals:
+    void shouldSwitchChanged();
+public slots:
+    void changeplayer();
+
+
 };
 #endif // WIDGET_H
